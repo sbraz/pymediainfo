86bced29740564bafd38bcd056b61457ebe35977
Chris Lucas <cjlucas07@gmail.com>
2011-09-19 01:24:29 -0700

    added: __repr__() in Track class

--- pymediainfo/__init__.py
+++ pymediainfo/__init__.py
@@ -59,6 +59,9 @@ class Track(object):
 						break
 					except:
 						pass
+					
+	def __repr__(self):
+		return("<Track track_id='{0}', track_type='{1}'>".format(self.track_id, self.track_type))
 
 	def to_data(self):
 		data = {}
