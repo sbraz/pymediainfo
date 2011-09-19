20696c19dda18a2977d85a8da879d332ad36ad5f
Chris Lucas <cjlucas07@gmail.com>
2011-09-19 01:20:12 -0700

    cleanup: python3 code

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
 from setuptools import setup, find_packages
 import sys
-if sys.version_info >= (3,0):
-    prereqs = []
+
+# simplejson isn't compatible with python3
+if sys.version_info >= (3,):
+	prereqs = []
 else:
-    prereqs = ['simplejson>=2.0.9',]
+	prereqs = ['simplejson>=2.0.9',]
 
 setup(
-    name='pymediainfo',
-    version = '1.3.4',
-    author='Patrick Altman',
-    author_email='paltman@gmail.com',
-    url='git@github.com/paltman/pymediainfo.git',
-    description="""A Python wrapper for the mediainfo command line tool.""",
-    packages=find_packages(),
-    namespace_packages = [],
-    include_package_data = True,
-    zip_safe=False,
-    license='MIT',
-    install_requires=prereqs,
+	name='pymediainfo',
+	version = '1.3.4',
+	author='Patrick Altman',
+	author_email='paltman@gmail.com',
+	url='git@github.com/paltman/pymediainfo.git',
+	description="""A Python wrapper for the mediainfo command line tool.""",
+	packages=find_packages(),
+	namespace_packages = [],
+	include_package_data = True,
+	zip_safe=False,
+	license='MIT',
+	install_requires=prereqs,
 )
