d6adb8c7ad0d38499e1508849416a156cecef1ff
Chris Lucas <cjlucas07@gmail.com>
2011-09-24 12:32:54 -0700

    converted tabs back to spaces

--- setup.py
+++ setup.py
@@ -3,21 +3,21 @@ import sys
 
 # simplejson isn't compatible with python3
 if sys.version_info >= (3,):
-	prereqs = []
+    prereqs = []
 else:
-	prereqs = ['simplejson>=2.0.9',]
+    prereqs = ['simplejson>=2.0.9', ]
 
 setup(
-	name='pymediainfo',
-	version = '1.3.4',
-	author='Patrick Altman',
-	author_email='paltman@gmail.com',
-	url='git@github.com/paltman/pymediainfo.git',
-	description="""A Python wrapper for the mediainfo command line tool.""",
-	packages=find_packages(),
-	namespace_packages = [],
-	include_package_data = True,
-	zip_safe=False,
-	license='MIT',
-	install_requires=prereqs,
+    name='pymediainfo',
+    version='1.3.4',
+    author='Patrick Altman',
+    author_email='paltman@gmail.com',
+    url='git@github.com/paltman/pymediainfo.git',
+    description="""A Python wrapper for the mediainfo command line tool.""",
+    packages=find_packages(),
+    namespace_packages=[],
+    include_package_data=True,
+    zip_safe=False,
+    license='MIT',
+    install_requires=prereqs,
 )
