dbea1f4fb5a1ad88b5dcfa51e56a86b4d6addf6e
Louis Sautier <sautier.louis@gmail.com>
2017-03-21 23:09:15 +0100

    Add Python 3.6 to classifiers

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Operating System :: POSIX :: Linux",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
