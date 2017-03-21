ea75bcd09cf101a829df4aa3e6394a537b20d3de
Louis Sautier <sautier.louis@gmail.com>
2017-03-21 23:11:52 +0100

    v2.1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pymediainfo',
-    version='2.1.6',
+    version='2.1.7',
     author='Louis Sautier',
     author_email='sautier.louis@gmail.com',
     url='https://github.com/sbraz/pymediainfo',
