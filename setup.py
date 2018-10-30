e1b0f07fad4940dfa5d87327f8ad28d30110d12a
bgermann <bgermann@users.noreply.github.com>
2018-10-30 15:19:21 +0100

    Enable shared library bundling, closes #61
    * CI is changed accordingly, only the required files are extracted
      from the upstream releases to prevent overwriting LICENSE or other files.
    * Travis CI is changed to use the libmediainfo-dev package (pulls the
      right libmediainfo0 or libmediainfo0v5 package depending on the
      distribution used).

--- setup.py
+++ setup.py
@@ -1,8 +1,35 @@
 #!/usr/bin/env python
+import os
 from setuptools import setup, find_packages
 
 with open("README.rst") as f:
     long_description = f.read()
+
+data_files = []
+bin_files = []
+cmdclass = {}
+
+bin_license = 'docs/License.html'
+if os.path.exists(bin_license):
+    data_files.append(('docs', [bin_license]))
+    bin_files.extend(['MediaInfo.dll', 'libmediainfo.*'])
+    try:
+        from wheel.bdist_wheel import bdist_wheel
+
+        class platform_bdist_wheel(bdist_wheel):
+            def finalize_options(self):
+                bdist_wheel.finalize_options(self)
+                # Force the wheel to be marked as platform-specific
+                self.root_is_pure = False
+            def get_tag(self):
+                python, abi, plat = bdist_wheel.get_tag(self)
+                # The python code works for any Python version,
+                # not just the one we are running to build the wheel
+                return 'py2.py3', 'none', plat
+
+        cmdclass['bdist_wheel'] = platform_bdist_wheel
+    except ImportError:
+        pass
 
 setup(
     name='pymediainfo',
@@ -16,10 +43,13 @@ setup(
     include_package_data=True,
     zip_safe=False,
     license='MIT',
+    data_files=data_files,
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
     install_requires=["setuptools"],
     tests_require=["pytest", "pytest-runner"],
+    package_data={'pymediainfo': bin_files},
+    cmdclass=cmdclass,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 2.6",
