2d1d81a0d3f8f7ebdc5beec1c7cc11d05bdedc46
Louis Sautier <sautier.louis@gmail.com>
2018-11-03 17:33:25 +0100

    Try to load libmediainfo from the module folder, refactor detection code

--- pymediainfo/__init__.py
+++ pymediainfo/__init__.py
@@ -140,20 +140,34 @@ class MediaInfo(object):
     @staticmethod
     def _get_library(library_file=None):
         os_is_nt = os.name in ("nt", "dos", "os2", "ce")
-        if library_file is not None:
-            if os_is_nt:
-                return WinDLL(library_file)
-            else:
-                return CDLL(library_file)
-        elif os_is_nt:
-            return windll.MediaInfo
-        elif sys.platform == "darwin":
-            try:
-                return CDLL("libmediainfo.0.dylib")
-            except OSError:
-                return CDLL("libmediainfo.dylib")
+        if os_is_nt:
+            lib_type = WinDLL
         else:
-            return CDLL("libmediainfo.so.0")
+            lib_type = CDLL
+        if library_file is None:
+            if os_is_nt:
+                library_names = ("MediaInfo.dll",)
+            elif sys.platform == "darwin":
+                library_names = ("libmediainfo.0.dylib", "libmediainfo.dylib")
+            else:
+                library_names = ("libmediainfo.so.0",)
+            script_dir = os.path.dirname(__file__)
+            # Look for the library file in the script folder
+            for library in library_names:
+                lib_path = os.path.join(script_dir, library)
+                if os.path.isfile(lib_path):
+                    # If we find it, don't try any other filename
+                    library_names = (lib_path,)
+                    break
+        else:
+            library_names = (library_file,)
+        for i, library in enumerate(library_names, start=1):
+            try:
+                return lib_type(library)
+            except OSError:
+                # If we've tried all possible filenames
+                if i == len(library_names):
+                    raise
     @classmethod
     def can_parse(cls, library_file=None):
         """
