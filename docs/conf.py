56543572e21ad20ff6a0ac699abbee5875d5813c
Louis Sautier <sautier.louis@gmail.com>
2025-02-11 01:10:18 +0100

    docs: drop source_suffix option
    According to
    https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-source_suffix,
    the default is {'.rst': 'restructuredtext'}.
    This silences the following warning:
    ```
    Converting `source_suffix = '.rst'` to `source_suffix = {'.rst': 'restructuredtext'}`.
    ```

--- docs/conf.py
+++ docs/conf.py
@@ -43,11 +43,6 @@ autodoc_typehints = "none"
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
-
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
 
